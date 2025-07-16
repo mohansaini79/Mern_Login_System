6693dd5461b0e713183b15e3ee855464e92f2707
mohansaini79 <mohansaini798330@gmail.com>
2025-07-16 20:12:14 +0530

    Update authroutes.js

--- backend/routes/authroutes.js
+++ backend/routes/authroutes.js
@@ -6,5 +6,8 @@ const { signup,login,googleLogin } = require("../controllers/authController");
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/google-login", googleLogin);
+router.get("/test"(req,res)=> {
+  res.sent("backend is working");
+});
 
 module.exports = router;
